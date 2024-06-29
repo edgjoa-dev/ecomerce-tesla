4a6bd707d6a80a90ed0648906f6dbcefa45f957a
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-06-28 19:42:28 -0600

    Dev TES-29 | 199-Side-Menú-Opciones-Icons: Adición de opciones de menú  en side menu

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 'use client'
+import Link from 'next/link'
 import React from 'react'
-import { IoCloseOutline, IoSearchOutline } from 'react-icons/io5'
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from 'react-icons/io5'
 
 export const Sidebar = () => {
     return (
         <div>
-        {/* Background Black */}
+            {/* Background Black */}
             <div
                 className='fixed top-0 w-screen h-screen z-10 bg-black opacity-25'
             />
@@ -21,18 +22,80 @@ export const Sidebar = () => {
                 <IoCloseOutline
                     size={30}
                     className='absolute top-5 right-5 cursor-pointer'
-                    onClick={ ()=> alert('Close') }
+                    onClick={() => alert('Close')}
                 />
 
                 {/* input search */}
                 <div className='relative mt-14'>
-                    <IoSearchOutline size={20} className='absolute top-2 left-2'/>
+                    <IoSearchOutline size={20} className='absolute top-2 left-2' />
                     <input
                         type='text'
                         placeholder='Buscar'
                         className='w-full rounded-md bg-gray-50 pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-400 hover:border-blue-200 transition-all'
                     />
                 </div>
+
+                {/* Opciones de Menu */}
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoPersonOutline size={30} />
+                    <span className="ml-3 text-xl">Perfil</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoTicketOutline size={30} />
+                    <span className="ml-3 text-xl">Ordenes</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoLogInOutline size={30} />
+                    <span className="ml-3 text-xl">Ingresar</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoLogOutOutline size={30} />
+                    <span className="ml-3 text-xl">Salir</span>
+                </Link>
+
+                {/* Line Separator */}
+                <div className="w-full h-px bg-gray-200 my-10" />
+
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoShirtOutline size={30} />
+                    <span className="ml-3 text-xl">Productos</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoTicketOutline size={30} />
+                    <span className="ml-3 text-xl">Ordenes</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+                >
+                    <IoPeopleOutline size={30} />
+                    <span className="ml-3 text-xl">Usuarios</span>
+                </Link>
+
             </nav>
         </div>
     )
