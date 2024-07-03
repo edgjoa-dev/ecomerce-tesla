1c5ed50acb90ac63c5857e863cc4c072fd64f83c
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-07-02 19:38:47 -0600

    TES-39-213-SlideShow Mobile: Creación de pantalla de slider responsive para version mobile

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,9 @@ export * from './products/product-grid/ProductGridItem'
 
 export * from './product/size-selector/SizeSelector'
 export * from './product/quantity-selector/QuantitySelector'
+
 export * from './product/slideshow/ProductSlideShow'
+export * from './product/slideshow/ProductMobileSlideShow'
 
 export * from './ui/sidebar/Sidebar'
 
