2af13c440dbd35d4f6fe9f1adce8ac66eaedff95
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2024-07-22 13:40:49 -0600

    TES-214 | Pantalla de carrito de conpras: Creacion inicial carrito de compras se agrego items con nombre de articulo, prelcio y selector de items además de coton de remover producto

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,8 +1,60 @@
+import { initialData } from '@/seed/seed';
+import { Title } from '../../../components/ui/title/Title';
+import Link from 'next/link';
+import Image from 'next/image';
+import { QuantitySelector } from '@/components';
+
+
+const productsCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+];
 
 export default function Cart() {
     return (
-        <section>
-            <h1>Cart Page</h1>
+        <section className="flex justify-center items-center mb-72 h-[600px] w-full px-10  sm:px-0" >
+            <div className="flex flex-col w-[1000px] p-3">
+
+                <Title title={'Carrito de Compras'} subtitle={''} className={''} />
+
+                <div className='grid grid-cols-1 sm:grid-cols-2 gap-10'>
+                    {/* carrito */}
+                    <div className='flex flex-col mt-5'>
+                        <span className='text-xl'> Agregar más items </span>
+                        <Link href='/' className='underline mb-5'> Continuar comprando</ Link>
+
+
+                        {/* Items cart */}
+                        {
+                            productsCart.map(({ slug, images, title, price }) => (
+                                <section key={slug} className='flex'>
+                                    <div>
+                                        <Image
+                                            src={`/products/${images[0]}`}
+                                            alt={title}
+                                            height={100}
+                                            width={100}
+                                            className='mr-5 rounded' />
+                                    </div>
+                                    <div>
+                                        <p>{title}</p>
+                                        <p>${price}</p>
+                                        <QuantitySelector quantity={3} />
+
+                                        <button className='underline mt-3'>
+                                            Remover
+                                        </button>
+                                    </div>
+                                </section>
+                            ))
+                        }
+                    </div>
+
+                    {/* Checkout */}
+                    <div className='flex flex-col justify-center items-center mt-5 p-3' >Checkout</div>
+                </div>
+            </div>
         </section>
     );
 }
