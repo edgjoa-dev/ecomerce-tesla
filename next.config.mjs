1d9b1f65e6b4cbb8e73da5fdd704dfd96e36d77f
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-07-30 15:11:52 -0600

    Creación de imagen de docker - ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        outputFileTracing: true,
+    },
+};
 
 export default nextConfig;
